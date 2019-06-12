f07772365ff9ed058082f30bdfee1aef75df4bc0
Moon Chiang <moonlove8310@gmail.com>
2019-06-12 18:33:39 +0800

    add Taiwan and USA

---
+++ driving.py
@@ -0,0 +1,15 @@
+country = input('what is your country? ')
+age = input('how old are you? ')
+age = int(age)
+if country == 'Taiwan':
+    if age >= 18:
+        print('you can have a driving test')
+    else:
+        print('you can not have a driving test')
+elif country == 'USA':
+    if age >= 16:
+        print('you can have a driving test')
+    else:
+        print('you can not have a driving test')
+else:
+    print('this program supports Taiwan and USA only')
